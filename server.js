a7d74383d2621eaa403c1081934f1a1bdf4b1e49
Dharmareddy p <19B61A0533@nmrec.edu.in>
2023-05-29 18:21:45 +0530

    firstsss

--- server.js
+++ server.js
@@ -26,6 +26,10 @@ app.get("/home", function (req, res) {
   res.send("hello world");
 });
 
+app.get("/", function (req, res) {
+  res.send("rootpage");
+});
+
 app.post("/create-payment-intent", async (req, res) => {
   const { items } = req.body;
 
